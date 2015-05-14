f3c7028c08d3de2ffbe7bd96edb0269428e6ba67
Ranga Vadhineni <ranga.vadhineni@hotmail.com>
2015-05-14 07:59:33 +0100

    ChatExample initial checkin to Github

---
+++ ChatExample/ChatExample/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PusherServer;
+
+namespace ChatExample.Controllers
+{
+    using System.Net;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PushMessage(string msg)
+        {
+            var pusher = new Pusher("[App ID]", "[App Key]", "[App Secret]");
+            var result = pusher.Trigger("test_channel", "new_message", new { message = msg });
+            return new HttpStatusCodeResult((int)HttpStatusCode.OK);
+        }
+
+        public ActionResult PrivateChannel()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PrivateMessage(string msg) 
+        {
+            var pusher = new Pusher("[App ID]", "[App Key]", "[App Secret]");
+            var result = pusher.Trigger("private-test_channel", "new_message", new { message = msg });
+            return new HttpStatusCodeResult((int)HttpStatusCode.OK);
+        }
+
+
+        public ActionResult Auth(string channel_name, string socket_id)
+        {
+            // ref:: https://pusher.com/docs/authenticating_users#/lang=cs-mvc
+            var pusher = new Pusher("[App ID]", "[App Key]", "[App Secret]");
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var auth = pusher.Authenticate(channel_name, socket_id);
+                var json = auth.ToJson();
+                return new ContentResult { Content = json, ContentType = "application/json" };
+            }
+
+            return new HttpStatusCodeResult((int)HttpStatusCode.Forbidden);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ChatExample/ChatExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatExample.Startup))]
+namespace ChatExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
